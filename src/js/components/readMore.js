3c3b2004edae8856a393fcf7129b857c553592fc
Roman <r.gutsu@bk.ru>
2019-12-17 15:00:23 +0200

    changed read-more buttons

--- src/js/components/readMore.js
+++ src/js/components/readMore.js
@@ -1,3 +1,4 @@
+
 var firstContentSection = document.querySelector('.firstspan');
 var secondContentSection = document.querySelector('.secondspan');
 var thirdContentSection = document.querySelector('.thirdspan');
@@ -5,38 +6,50 @@ var firstButton = document.getElementById('cta1');
 var secondButton = document.getElementById('cta2');
 var thirdButton = document.getElementById('cta3');
 
-function firstReadMoreFunction() {
-  if (firstContentSection.style.display === '' || firstContentSection.style.display === 'none') {
-    firstContentSection.style.display = 'inline';
+function firstButtonFunction() {
+  if (firstContentSection.classList.contains('hide')) {
+    firstContentSection.classList.remove('hide');
+    firstContentSection.classList.add('show');
     firstButton.innerHTML = 'READ LESS';
   } else {
-    firstContentSection.style.display = 'none';
+    firstContentSection.classList.add('hide');
+    firstContentSection.classList.remove('show');
     firstButton.innerHTML = 'READ MORE';
   }
 }
 
-firstButton.addEventListener('click', firstReadMoreFunction);
-
-function secondReadMoreFunction() {
-  if (secondContentSection.style.display === '' || secondContentSection.style.display === 'none') {
-    secondContentSection.style.display = 'inline';
+function secondButtonFunction() {
+  if (secondContentSection.classList.contains('hide')) {
+    secondContentSection.classList.remove('hide');
+    secondContentSection.classList.add('show');
     secondButton.innerHTML = 'READ LESS';
   } else {
-    secondContentSection.style.display = 'none';
+    secondContentSection.classList.add('hide');
+    secondContentSection.classList.remove('show');
     secondButton.innerHTML = 'READ MORE';
   }
 }
 
-secondButton.addEventListener('click', secondReadMoreFunction);
-
-function thirdReadMoreFunction() {
-  if (thirdContentSection.style.display === '' || thirdContentSection.style.display === 'none') {
-    thirdContentSection.style.display = 'inline';
+function thirdButtonFunction() {
+  if (thirdContentSection.classList.contains('hide')) {
+    thirdContentSection.classList.remove('hide');
+    thirdContentSection.classList.add('show');
     thirdButton.innerHTML = 'READ LESS';
   } else {
-    thirdContentSection.style.display = 'none';
+    thirdContentSection.classList.add('hide');
+    thirdContentSection.classList.remove('show');
     thirdButton.innerHTML = 'READ MORE';
   }
 }
 
-thirdButton.addEventListener('click', thirdReadMoreFunction);
+function readMore(e) {
+  if (e.target.classList.contains('cta1')) {
+    firstButtonFunction();
+  } else if (e.target.classList.contains('cta2')) {
+    secondButtonFunction();
+  } else if (e.target.classList.contains('cta3')) {
+    thirdButtonFunction();
+  }
+}
+
+document.querySelector('.section-blog').addEventListener('click', readMore);
